e2ed455e9b0893f6f1a21a13350fe19b222fe6f4
Xinwei Ge <xinwei.ge97@gmail.com>
2020-08-23 19:56:57 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+
+
+
+from riotwatcher import LolWatcher, ApiError
+
+
+
+def requestSummonerData():
+
+    summonerName = input("what is your IGN")
+    region = input("what is your region")
+
+    return (summonerName,region)
+
+def main():
+
+    root = Tk()
+
+
+    region = "na"
+    summonerName = "Top Never Tilts"
+
+    api_key = "RGAPI-f15f6ba5-76bd-4cd2-a63f-ff7937955946"
+    watcher = LolWatcher(api_key)
+    my_region = region + "1"
+    me = watcher.summoner.by_name(my_region, summonerName)
+
+    my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+
+    theLabel = Label(root, text = my_ranked_stats[0]['tier'])
+    theLabel.pack()
+    root.mainloop()
+
+    print(my_ranked_stats)
+    print(my_ranked_stats[0]['tier'])
+    print(me)
+
+
+# This starts my program!
+if __name__ == "__main__":
+    main()
